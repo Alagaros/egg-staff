76b99f4aabcd71cfeed3280ea13f2d031eeeba34
Trevi Awater <awatertrevi@gmail.com>
2015-08-02 21:12:31 +0200

    Changed mod id

--- src/main/java/com/dalthow/eggstaff/base/Reference.java
+++ src/main/java/com/dalthow/eggstaff/base/Reference.java
@@ -16,5 +16,5 @@ public class Reference
 	public final static String name = "Egg Staff";
 	public final static String version = "0.2.0.0";
 	
-	public final static String modId = "eggstaff";
+	public final static String modId = "egg-staff";
 }
